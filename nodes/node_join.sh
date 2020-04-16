68c407c887ebedd01b63b8f39e8e9723d0ec3d50
rahulwaykos <60727515+rahulwaykos@users.noreply.github.com>
2020-04-16 11:54:44 +0530

    Create node_join.sh

---
+++ nodes/node_join.sh
@@ -0,0 +1,13 @@
+
+file = k8s_repo
+
+if [ -f $file ] 
+then
+chmod +x $file
+./$file
+else 
+echo -e "Change your working directory where following files are present\n
+\t 1. k8s_repo\n
+\t 2. token\n
+\t 3. node_join.sh"
+fi
